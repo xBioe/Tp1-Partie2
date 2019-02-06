685e1e9903a6595c04525539732a0a5871914c7d
Ilyas Elamraoui <wdpraw@gmail.com>
2019-02-06 11:53:24 -0500

    Ajout des Constructeur/Accesseur/Mutateur de la classe Plat

--- src/Plat.java
+++ src/Plat.java
@@ -1,4 +1,28 @@
 
 public class Plat {
 
+	private String plat;
+	private double prix;
+	
+	public Plat(String plat, double prix) {
+		this.plat=plat;
+		this.prix=prix;
+		
+	}
+	
+	public String getPlat() {
+		return plat;
+	}
+	
+	public void setPlat(String plat) {
+		this.plat=plat;
+	}
+	
+	public double getPrix() {
+		return prix;
+	}
+	
+	public void setPrix(double prix) {
+		this.prix=prix;
+	}
 }
