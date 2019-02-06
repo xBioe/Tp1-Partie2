e7be8afc882bee61e26830dcd98347e1de512563
Ilyas Elamraoui <wdpraw@gmail.com>
2019-02-06 11:48:06 -0500

    Ajout des Constructeur/Accesseur/Mutateur de la classe Client

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,23 @@
+import java.util.ArrayList;
 
 public class Client {
+	private String nom;
+	private ArrayList<Commande> listeCommande;
+	
+	public Client(String nom) {
+		this.nom = nom;
+	}
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void setNom(String nom) {
+		this.nom=nom;
+	}
+	
+	public ArrayList<Commande> getListeCommande(){
+		return listeCommande;
+	}
 
 }
