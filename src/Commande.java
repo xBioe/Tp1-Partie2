f1a2f0b403813287e5ffef3cb3a2a953e1755f6d
Ilyas Elamraoui <wdpraw@gmail.com>
2019-02-06 12:00:19 -0500

    Ajout des Constructeur/Accesseur/Mutateur de la classe Commande

--- src/Commande.java
+++ src/Commande.java
@@ -1,4 +1,38 @@
 
 public class Commande {
+	
+	private Plat plat;
+	private int quantite;
+	private String nomClient;
+	
+	public Commande(Plat plat, int quantite, String nomClient) {
+		this.plat=plat;
+		this.quantite=quantite;
+		this.nomClient=nomClient;
+	}
+	
+	public Plat getPlat() {
+		return plat;
+	}
+	
+	public void setPlat(Plat plat) {
+		this.plat=plat;
+	}
+	
+	public int getQuantite() {
+		return quantite;
+	}
+	
+	public void setQuantite(int quantite) {
+		this.quantite=quantite;
+	}
+	
+	public String getNomClient() {
+		return nomClient;
+	}
+	
+	public void setNomClient(String nomClient) {
+		this.nomClient=nomClient;
+	}
 
 }
